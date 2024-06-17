c872629afff9609df0aa34cbcd98b6dedf52053e
Hascine <thehascine@gmail.com>
2024-06-18 04:42:08 +0700

    feat: profile, edit profile

---
+++ src/app/(public)/profile/edit/page.tsx
@@ -0,0 +1,11 @@
+import EditProfileForm from '@/components/EditProfileForm';
+
+const EditProfilePage = () => {
+  return (
+    <div className="m-10  rounded-md shadow-md">
+      <EditProfileForm />
+    </div>
+  );
+};
+
+export default EditProfilePage;
