637efb4704f29ed83307a59f2314935e51c1b17e
Hascine <thehascine@gmail.com>
2024-06-19 10:18:19 +0700

    add blank profile feature

--- src/app/(public)/profile/page.tsx
+++ src/app/(public)/profile/page.tsx
@@ -50,7 +50,10 @@ export default async function ProfilePage() {
       <section className="flex flex-col md:flex-row rounded-lg shadow-lg m-5 lg:mx-40 justify-center">
         <div className="flex md:w-1/2">
           <picture className="w-full md:h-96 m-auto">
-            <img className="rounded-xl w-full h-72 md:h-96 object-cover" src={profile?.data?.profile?.image} />
+            <img
+              className="rounded-xl w-full h-72 md:h-96 object-cover"
+              src={profile?.data?.profile?.image || '/blank-profile.jpg'}
+            />
           </picture>
         </div>
         <div className="flex flex-col p-5 md:p-10 md:w-1/2">
